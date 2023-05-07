a0ed0d100689f34bc7d58f025ed23599557e1d30
floydfdes <floydfernandes20@gmail.com>
2023-05-07 20:21:38 +0530

    Port changed to work in mac os

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ console.log("connected to api");
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 /**
  * security:
