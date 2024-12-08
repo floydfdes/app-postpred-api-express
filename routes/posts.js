90170e2783c2bd3e4c43b957a046ed5a68a0580e
floydfdes <floydfernandes20@gmail.com>
2024-12-08 12:26:45 +0530

    Swagger doc for get post by id

--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,8 @@ import {
   updatePost,
 } from "../controllers/posts.js";
 
-import { authentication } from "./validateToken.js";
 import express from "express";
+import { authentication } from "./validateToken.js";
 
 const router = express.Router();
 /**
@@ -69,6 +69,115 @@ router.get("/posts/getAllPosts", getAllPosts);
  *       description: post was created
  */
 router.post("/", authentication, createPost);
+
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get a single post by ID
+ *     tags: [Hobbies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The unique ID of the post
+ *     responses:
+ *       '200':
+ *         description: Successfully retrieved the post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The unique ID of the post
+ *                 title:
+ *                   type: string
+ *                   description: Title of the post
+ *                 description:
+ *                   type: string
+ *                   description: Description of the post
+ *                 tags:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   description: Tags associated with the post
+ *                 creatorName:
+ *                   type: string
+ *                   description: Name of the creator
+ *                 creator:
+ *                   type: string
+ *                   description: The user ID of the creator
+ *                 likes:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   description: Array of user IDs who liked the post
+ *                 comments:
+ *                   type: array
+ *                   description: Comments on the post
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       content:
+ *                         type: string
+ *                         description: Content of the comment
+ *                       userId:
+ *                         type: string
+ *                         description: The user ID of the commenter
+ *                       likes:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             _id:
+ *                               type: string
+ *                               description: User ID who liked the comment
+ *                             name:
+ *                               type: string
+ *                               description: Name of the user
+ *                             avatarUrl:
+ *                               type: string
+ *                               description: Avatar URL of the user
+ *                       dislikes:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             _id:
+ *                               type: string
+ *                               description: User ID who disliked the comment
+ *                             name:
+ *                               type: string
+ *                               description: Name of the user
+ *                             avatarUrl:
+ *                               type: string
+ *                               description: Avatar URL of the user
+ *                       likesCount:
+ *                         type: integer
+ *                         description: Number of likes on the comment
+ *                       dislikesCount:
+ *                         type: integer
+ *                         description: Number of dislikes on the comment
+ *                       userLiked:
+ *                         type: boolean
+ *                         description: Whether the current user liked the comment
+ *                       userDisliked:
+ *                         type: boolean
+ *                         description: Whether the current user disliked the comment
+ *                 date:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Date when the post was created
+ *       '404':
+ *         description: Post not found
+ *       '500':
+ *         description: Server error
+ */
 router.get("/:id", getPost);
 
 /**
